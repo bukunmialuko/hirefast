955d0f145c184dcae7fa7d3a9e50ff61642b4616
vikassandhu999 <vikassandhu999@gmail.com>
2021-08-27 16:49:01 +0530

    green : AddQuestion

--- backend/src/hiring/usecases/add-question/AddQuestion.usecase.ts
+++ backend/src/hiring/usecases/add-question/AddQuestion.usecase.ts
@@ -1,14 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { hasValue } from 'src/@shared/core/has-value';
 import { UseCase } from 'src/@shared/core/usecase';
+import {
+  IInterviewsRepository,
+  INTERVIEWS_REPOSITORY,
+} from 'src/hiring/details/IInterviewsRepository';
 import { AddQuestionInput } from 'src/hiring/usecases/add-question/AddQuestionInput.dto';
 import { AddQuestionResponse } from 'src/hiring/usecases/add-question/AddQuestionResponse';
+import { InvalidInterviewIdError } from 'src/hiring/usecases/add-question/InvalidInterviewId.error';
 
 @Injectable()
 export class AddQuestionUseCase extends UseCase<
   AddQuestionInput,
   AddQuestionResponse
 > {
-  async run(dto: AddQuestionInput): Promise<AddQuestionResponse> {
+  constructor(
+    @Inject(INTERVIEWS_REPOSITORY)
+    private interviewsRepository: IInterviewsRepository,
+  ) {
+    super();
+  }
+
+  async run(input: AddQuestionInput): Promise<AddQuestionResponse> {
+    const interviewId = input.interviewId;
+
+    const interview = await this.interviewsRepository.findById(interviewId);
+
+    if (!hasValue(interview)) {
+      throw new InvalidInterviewIdError();
+    }
+
     return Promise.resolve(undefined);
   }
 }
